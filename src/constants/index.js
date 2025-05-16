93bd1bdea76c155cf34cc63d6b6af4b04fc153de
Iago Pierre <pierrestorniolo@gmail.com>
2025-05-15 22:57:15 -0300

    added constants folder and animation to hero section

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { text: "Ideias", imgPath: "/images/ideas.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Conceitos", imgPath: "/images/concepts.svg" },
+  { text: "Código", imgPath: "/images/code.svg" },
+  { text: "Ideias", imgPath: "/images/ideas.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Conceitos", imgPath: "/images/concepts.svg" },
+  { text: "Código", imgPath: "/images/code.svg" },
+];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import { words } from "../constants/index.js";
 
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layot">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Transformando
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>em projetos reais</h1>
+              <h1>que entregam resultados</h1>
+            </div>
+          </div>
+        </header>
       </div>
     </section>
   );
