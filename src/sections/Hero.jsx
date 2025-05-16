26909263afcdc27f0765031a806f782db08a4bb0
Iago Pierre <pierrestorniolo@gmail.com>
2025-05-15 22:29:06 -0300

    inital commit portfolio

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
